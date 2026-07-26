05ecfe38eb38d34c7ce299d1a68faedc353655e1
swapnildwivedi <dwivediswapnil005@gmail.com>
2026-07-26 14:20:32 +0530

    Initial commit -- Python basics

---
+++ hello_chai.py
@@ -0,0 +1,24 @@
+massage ="hello world"
+
+#name = "Swapnil"
+print(massage)
+
+#for mas_point in massage:
+#	print(mas_point)
+
+print(massage.count("l"))
+print(massage.find("world"))
+
+replaced_massage = massage.replace("world" , "universe")
+
+print(replaced_massage)
+
+
+greeting = "hello"
+name = 'swapnil'
+
+massage_greet = '{}, {}. Welcome!!'.format(greeting , name)
+
+print(massage_greet)
+
+print(help(str))
---
+++ numbers_test.py
@@ -0,0 +1,8 @@
+num = 3
+
+print(type(num)) #This type() function shows the datatype of the object  OUTPUT:: <class 'int'>
+
+float_num = 3.14
+
+print(type(float_num)) # now it is showing that the datatype is float OUTPUT:: <class 'float'>
+
